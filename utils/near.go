c34365290dbfc10502361dec62d6f513eb163b69
David <mazanax@gmail.com>
2022-04-25 20:56:53 +0300

    Fix passing node_env to near cli command

--- utils/near.go
+++ utils/near.go
@@ -9,9 +9,10 @@ import (
 
 func IsNearWalletValid(wallet string) bool {
 	cmd := exec.Command("near", "state", wallet)
-	cmd.Env = []string{"NODE_ENV", config.NearNetwork}
-	_, err := cmd.Output()
+	cmd.Env = []string{"NEAR_ENV=" + config.NearNetwork}
+	output, err := cmd.Output()
 	if err != nil {
+		fmt.Printf("Err: %s, output: %s, env: %s\n", err, output, config.NearNetwork)
 		return false
 	}
 
@@ -22,7 +23,7 @@ func Withdraw(wallet string, amount uint64, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	cmd := exec.Command("near", "send", config.NearWallet, wallet, fmt.Sprintf("%.5f", float64(amount)/1e5))
-	cmd.Env = []string{"NODE_ENV", config.NearNetwork}
+	cmd.Env = []string{"NODE_ENV=" + config.NearNetwork}
 	err := cmd.Run()
 	if err != nil {
 		return
